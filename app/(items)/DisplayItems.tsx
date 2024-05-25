557ddc88a377dc5a926c1f9e592ea1ee4ac2cde8
OGEmanuel <1blacksanta416@gmail.com>
2024-05-25 23:22:32 +0100

    Justify added to items

--- app/(items)/DisplayItems.tsx
+++ app/(items)/DisplayItems.tsx
@@ -4,7 +4,7 @@ import { ItemDialog } from "./ItemDialog";
 const DisplayItems = () => {
   return (
     <Card className="p-6 md:p-8">
-      <div className="grid w-full grid-cols-[repeat(2,_minmax(auto,_17rem))] gap-5 overflow-auto md:grid-cols-[repeat(auto-fit,_minmax(auto,_17rem))] md:gap-8">
+      <div className="grid w-full grid-cols-[repeat(2,_minmax(auto,_17rem))] gap-5 overflow-auto md:grid-cols-[repeat(auto-fit,_minmax(auto,_17rem))] md:justify-center md:gap-8">
         <ItemDialog />
         <ItemDialog />
         <ItemDialog />
